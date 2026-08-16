22cc8dce1af1d6b08b20f766598ba16363c74242
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-08-17 01:06:46 +0800

    feat: add greet and exit message

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,10 +1,26 @@
 public class Bob {
     public static void main(String[] args) {
+        String chatBotName = "Bob";
+        String divider = "____________________________________________________________";
+        System.out.println(divider);
+
+        // @formatter:off
         String banner = " ____        _     \n"
                 + "| __ )  ___ | |__\n"
                 + "|  _ \\ / _ \\| '_ \\\n"
                 + "| |_) | (_) | |_) |\n"
-                + "|____/ \\___/|_.__/\n";
+                + "|____/ \\___/|_.__/";
+        // @formatter:on
         System.out.println(banner);
+
+        String helloMessage = "Hello! I'm " + chatBotName + ".\n" + "What can I do for you?";
+        System.out.println(helloMessage);
+
+        System.out.println(divider);
+
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(goodbyeMessage);
+
+        System.out.println(divider);
     }
 }
