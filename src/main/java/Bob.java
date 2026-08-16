6524081dbcd4ff4893265309ae9420a0497c9777
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-08-17 00:40:51 +0800

    feat: rename chatbot from Duke to Bob

---
+++ src/main/java/Bob.java
@@ -0,0 +1,10 @@
+public class Bob {
+    public static void main(String[] args) {
+        String banner = " ____        _     \n"
+                + "| __ )  ___ | |__\n"
+                + "|  _ \\ / _ \\| '_ \\\n"
+                + "| |_) | (_) | |_) |\n"
+                + "|____/ \\___/|_.__/\n";
+        System.out.println(banner);
+    }
+}
