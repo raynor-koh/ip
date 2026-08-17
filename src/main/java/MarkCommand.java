eaeb72e2db29d6f76a3615c2f3c6a48c5f3dba53
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-08-18 02:37:02 +0800

    feat: add mark command parsing and execution

---
+++ src/main/java/MarkCommand.java
@@ -0,0 +1,17 @@
+/**
+ * Command that marks a task as done.
+ */
+public class MarkCommand extends Command {
+    private final int taskNumber;
+
+    public MarkCommand(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    @Override
+    public void execute(Ui ui, TaskList taskList) {
+        Task task = taskList.get(taskNumber - 1);
+        task.markAsDone();
+        ui.showMarked(task);
+    }
+}
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -10,6 +10,12 @@ public class Parser {
         if (input.equals("list")) {
             return new ListCommand();
         }
+
+        String[] words = input.split(" ", 2);
+        if (words[0].equals("mark")) {
+            int taskNumber = Integer.parseInt(words[1].trim());
+            return new MarkCommand(taskNumber);
+        }
         return new AddCommand(input);
     }
 }
