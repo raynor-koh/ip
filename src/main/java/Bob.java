4239441792526ddde0dec867ca16ff36095824b4
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-08-18 00:56:52 +0800

    refactor: move Bob entry point to start Chatbot

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,41 +1,14 @@
-import java.util.Scanner;
-
+/**
+ * Entry point for the Bob chatbot application.
+ */
 public class Bob {
+    /**
+     * Starts the chatbot.
+     *
+     * @param args command-line arguments, currently unused
+     */
     public static void main(String[] args) {
-        String chatBotName = "Bob";
-        String linePrefix = "    ";
-        String messagePrefix = "     ";
-        String divider = linePrefix + "____________________________________________________________";
-        System.out.println(divider);
-
-        // @formatter:off
-        String banner = messagePrefix + " ____        _     \n"
-                + messagePrefix + "| __ )  ___ | |__\n"
-                + messagePrefix + "|  _ \\ / _ \\| '_ \\\n"
-                + messagePrefix + "| |_) | (_) | |_) |\n"
-                + messagePrefix + "|____/ \\___/|_.__/";
-        // @formatter:on
-        System.out.println(banner);
-
-        String helloMessage = messagePrefix + "Hello! I'm " + chatBotName + ".\n" + messagePrefix
-                                        + "What can I do for you?";
-        System.out.println(helloMessage);
-
-        System.out.println(divider);
-
-        String goodbyeMessage = "Bye. Hope to see you again soon!";
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String command = scanner.nextLine();
-            System.out.println(divider);
-            if (command.equals("bye")) {
-                System.out.println(messagePrefix + goodbyeMessage);
-                System.out.println(divider);
-                break;
-            }
-            System.out.println(messagePrefix + command);
-            System.out.println(divider);
-        }
-        scanner.close();
+        ChatBot chatBot = new ChatBot();
+        chatBot.run();
     }
 }
