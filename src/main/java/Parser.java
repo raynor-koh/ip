e22f62995454b57d5d3da1850d3a5dd8f14b9d0b
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-08-19 02:26:06 +0800

    feat: parse command into specific task types

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -20,6 +20,28 @@ public class Parser {
             int taskNumber = Integer.parseInt(words[1].trim());
             return new UnmarkCommand(taskNumber);
         }
-        return new AddCommand(input);
+        if (words[0].equals("todo")) {
+            return new AddCommand(new ToDo(words[1]));
+        }
+        if (words[0].equals("deadline")) {
+            String[] parts = words[1].split(" /by ", 2);
+
+            String description = parts[0];
+            String by = parts[1];
+
+            return new AddCommand(new Deadline(description, by));
+        }
+        if (words[0].equals("event")) {
+            String[] fromParts = words[1].split(" /from ", 2);
+            String description = fromParts[0];
+
+            String[] toParts = fromParts[1].split(" /to ", 2);
+            String from = toParts[0];
+            String to = toParts[1];
+
+            return new AddCommand(new Event(description, from, to));
+        }
+        // Fallback Case: Will implement replacement later
+        return new AddCommand(new ToDo(input));
     }
 }
